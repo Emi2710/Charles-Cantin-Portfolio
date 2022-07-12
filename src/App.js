a4da89c7c76a09918a304fd1d399fceffe390a48
Emi2710 <deadourfreedom@gmail.com>
2022-07-12 22:50:57 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import { Navbar } from './components';
-import { Footer } from './container';
+import { Home, Gallery, Price, Footer } from './container';
 
 const App = () => {
+
+  const showPage = () => {
+        if (window.location.pathname === "/" || window.location.pathname === "/accueil"  ) {
+            return <Home />
+        } else if (window.location.pathname === "/gallerie") {
+            return <Gallery />
+        } else if (window.location.pathname === "/tarifs") {
+            return <Price />
+        }    
+    }
   return (
       
         <div className='app'>
             <Navbar />
+            {showPage()}  
             <Footer />
+
         </div>    
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,52 +3,47 @@ import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
 import './Navbar.scss';
-import { Home, Gallery, Price } from '../../container';
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
-    const showPage = () => {
-        if (window.location.pathname === "/" || window.location.pathname === "/accueil"  ) {
-            return <Home />
-        } else if (window.location.pathname === "/gallerie") {
-            return <Gallery />
-        } else if (window.location.pathname === "/tarifs") {
-            return <Price />
-        }    
-    }
-
     
   return (
-    <nav className='app__navbar'>
-        
-        <ul className='app__navbar-links'>
-            <li><a href='accueil'>Accueil</a></li>
-            <li><a href='gallerie'>Gallerie</a></li>
-            <li><a href='tarifs'>Tarifs</a></li>
-        </ul>
+    <div>
+      <nav className='app__navbar'>
+          
+          <ul className='app__navbar-links'>
+              <li className="app__flex p-text"><a href='accueil'>Accueil</a></li>
+              <li className="app__flex p-text"><a href='gallerie'>Gallerie</a></li>
+              <li className="app__flex p-text"><a href='tarifs'>Tarifs</a></li>
+          </ul>
+          <button>Contactez-moi</button>
 
-        <div className='app__navbar-menu'>
-            <HiMenuAlt4 onClick={() => setToggle(true)} />
+          <div className='app__navbar-menu'>
+              <HiMenuAlt4 onClick={() => setToggle(true)} className='hamburger' />
 
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-                <li><a href='accueil'>Accueil</a></li>
-                <li><a href='gallerie'>Gallerie</a></li>
-                <li><a href='tarifs'>Tarifs</a></li>
-            </ul>
-          </motion.div>
-        )}
-        </div>
+          {toggle && (
+            <motion.div 
+            whileInView={{x: [-300, 0]}}
+            transition={{duration: 0.85, ease: 'easeOut'}}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul className='app__navbar-toggle'>
+                  <li><a href='accueil'>Accueil</a></li>
+                  <li><a href='gallerie'>Gallerie</a></li>
+                  <li><a href='tarifs'>Tarifs</a></li>
+              </ul>
+            </motion.div>
+          )}
+          </div>
+  
+      </nav>  
 
-        {showPage()}
-        
-    </nav>
+      
+
+    </div>
+    
+    
   )
 }
 
