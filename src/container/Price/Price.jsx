d414e8868f1d84985a7a1759c70d107c055c5ff3
Emi2710 <deadourfreedom@gmail.com>
2022-07-15 16:24:58 +0300

    tarifs page

--- src/container/Price/Price.jsx
+++ src/container/Price/Price.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import { urlFor, client } from '../../client';
+
+import './Price.scss';
 
 const Price = () => {
+
+  const [price, setPrice] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "price"]';
+
+    client.fetch(query).then((data) => {
+      setPrice(data);
+    });
+  }, []);
+
   return (
-    <div>Price</div>
+    <div className='app__price-wrapper'>
+      <h1 className='title' style={{color:'#fff', margin:'2rem'}}>Les tarifs</h1>
+      <div className='app__price-flex'>
+        {price.map((price, index) => (
+          <>
+              <div className='app__price-container'>
+                <h2 className='title'>{price.titre}</h2>
+                <p>{price.description}</p>
+                <h2 className='title'>{price.prix}</h2>
+              </div>
+          </>
+        ))}  
+      </div>
+      
+    </div>
   )
 }
 
--- src/container/index.js
+++ src/container/index.js
@@ -1,4 +1,3 @@
 export { default as Home } from './Home/Home';
 export { default as Gallery } from './Gallery/Gallery';
 export { default as Price } from './Price/Price';
-export { default as Footer } from './Footer/Footer';
